ed5852114b9019bbb1dda59c886ea013b2d17f1a
anapaulaa <ana.paula.angpz@gmail.com>
2022-11-01 10:44:45 -0600

    Primeros archivos de bootstrap, ccs avanzado hasta intro web7 y js2

---
+++ intro-web-7/script.js
@@ -0,0 +1,100 @@
+//hola
+
+/*console.log("Ana");
+
+alert("Alerta de chisme");
+
+var numero= 5556955490 ;
+var palabra= "Ana Paula";
+var mascota= true;
+var trabajo=false; 
+var sueldo=null;
+
+console.log("Hola CH20");
+console.log(String("Hola CH20"));
+
+console.log(123445678);
+console.log(String(123445678));
+console.log(Number(123445678));
+console.log(Boolean(123445678));
+
+console.log(true);
+console.log(String(true));
+console.log(Number(true));
+console.log(Boolean(true)); 
+
+var anioAlan = 1996;
+var anioDavid = 1993;
+var anioAndres = 1998;
+var anioAna = 1998;
+var anioOlivia = 1992;
+var anioMontiel = 1999;
+var anioActual = 2022;
+
+console.log('la edad de Alan es', anioActual - anioAlan);
+console.log('la edad de David es',anioActual - anioDavid);
+console.log('la edad de Andres es',anioActual - anioAndres);
+console.log('la edad de Ana es',anioActual - anioAna);
+console.log('la edad de Olivia es',anioActual - anioOlivia);
+console.log('la edad de Montiel es',anioActual - anioMontiel);*/
+
+
+console.log(Number(false));
+console.log(String(false));
+console.log(Boolean(false));
+
+console.log(Number(true));
+console.log(String(true));
+console.log(Boolean(true));
+
+console.log(Number(0));
+console.log(String(0));
+console.log(Boolean(0));
+
+console.log(Number(1));
+console.log(String(1));
+console.log(Boolean(1));
+
+console.log(Number("0"));
+console.log(String("0"));
+console.log(Boolean("0"));
+
+console.log(Number("000"));
+console.log(String("000"));
+console.log(Boolean("000"));
+
+console.log(Number("1"));
+console.log(String("1"));
+console.log(Boolean("1"));
+
+console.log(Number(NaN));
+console.log(String(NaN));
+console.log(Boolean(NaN));
+
+console.log(Number(Infinity));
+console.log(String(Infinity));
+console.log(Boolean(Infinity));
+
+console.log(Number(-Infinity));
+console.log(String(-Infinity));
+console.log(Boolean(-Infinity));
+
+console.log(Number(""));
+console.log(String(""));
+console.log(Boolean(""));
+
+console.log(Number("20"));
+console.log(String("20"));
+console.log(Boolean("20"));
+
+console.log(Number("twenty"));
+console.log(String("twenty"));
+console.log(Boolean("twenty"));
+
+console.log(Number(null));
+console.log(String(null));
+console.log(Boolean(null));
+
+console.log(Number(undefinited));
+console.log(String(undefinited));
+console.log(Boolean(undefinited));
---
+++ js-02/funcionesJS.js
@@ -0,0 +1,117 @@
+/*let miNom = "Ana Paula";
+console.log(miNombre);*/
+
+function miNombre(){
+    let nombre= "Paula";
+    console.log(nombre);
+}
+
+miNombre ();
+
+function funcionUno(){
+    let valor = 2;
+    valor = valor +3;
+    return valor;
+}
+
+let resultado = funcionUno();
+console.log("el resultado es: ", resultado);
+
+function funcionEjemplo(){
+    let valor1 =2;
+    valor1 = valor1 + 3;
+    console.log("El resultado de valor1: " + valor1);
+}
+
+funcionEjemplo();
+
+//variable global
+let variableGlobal = 7;
+function funcion3(){
+    let variablelocal = "El valor es: "
+    console.log(variablelocal + variableGlobal);
+}
+
+funcion3();
+   
+//Parametros
+function funcion4 (valor){
+    let numero = 6;
+    numero = numero * valor;
+    return numero;
+}
+let salida = funcion4(3);
+console.log("El producto es: ", salida);
+
+//funciones anonimas
+let miFuncion0 = function(parametro){
+    parametro = parametro * 2;
+    return parametro;
+}
+console.log("Llamando a la funcion anonima "+ miFuncion0 (4));
+
+let funcion01 = function (caracteres){
+    return "Hola "+ caracteres;
+}("Lunes");
+console.log("El aviso es: "+ funcion01);
+
+//funcion flecha
+let suma = (x, y) =>{ return x+y};
+let resultadof = suma(5, 3);
+console.log("La suma es:", resultadof);
+
+//Ejercicio Calcular el precio total de una compra
+
+function precioTotal(precio) {
+   
+    let impuesto = 1.16;
+    let envio = 10;
+    let total = (precio*impuesto)+envio;
+    console.log(total);
+}
+precioTotal(23.34);
+
+/*let precioTotal = function (precio) {
+   
+    let impuesto = 1.16;
+    let envio = 10;
+    return (precio*impuesto)+envio;
+   
+}(23.34);
+console.log("El precio es: "+ precioTotal);
+*/
+//DECLARACION DE OBJETOS
+let miObjeto = {
+    nombre: "Ana",
+    edad: 10,
+    datos: function (){
+        let mensaje = "tu nombre es: " + this.nombre;
+        mensaje += " tienes " + this.edad + " anios";
+        console.log(mensaje);
+    }
+};
+miObjeto.datos();
+//constructor objetos
+let constructor = function(){
+    let objPersona = {
+        nombre: "Juan",
+        nss: 2357,
+        datosPersona: function (){
+            let msj= "Eres " + this.nombre + " tu numero nss es:" + this.nss;
+            console.log(msj);
+        },
+        otroMetodo: function (parametro){
+            let numero = 8;
+            console.log("La suma es : "+ (numero+parametro));
+        }
+    };
+    return objPersona;
+}
+//para la llamada del constructor primero llamamos al objeto y despues a los parametros
+let varObjReg = constructor()//crear la variable llamada objeto
+//dentro de un objeto
+varObjReg.datosPersona();
+varObjReg.otroMetodo(5);
+
+
+
